97609848dccb739055aae06bd5e8fd6909a5042f
Kelwin2712 <kelwinrocha5@gmail.com>
2025-09-01 21:24:32 -0300

    new navbar

--- estruturas/navbar/navbar-compras.php
+++ estruturas/navbar/navbar-compras.php
@@ -3,7 +3,7 @@
     <div class="container-fluid px-lg-5">
       <a class="navbar-brand" href="index.php">
         <img src="./img/logo-fahren.png" alt="Logo" width="15" height="20" class="d-inline-block align-text-center" style="filter: invert(1);">
-        Fahren
+        <span class="ms-1 fw-semibold">Fahren</span>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
