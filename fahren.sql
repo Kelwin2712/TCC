2dff8293f1f2bef0e93cec66af0bf45f8ba1cee1
Kelwin2712 <kelwinrocha5@gmail.com>
2025-10-01 22:17:27 -0300

    bd

--- fahren.sql
+++ fahren.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26/09/2025 às 04:07
+-- Tempo de geração: 02/10/2025 às 03:16
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `fahren`
 --
+CREATE DATABASE IF NOT EXISTS `fahren` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `fahren`;
 
 -- --------------------------------------------------------
 
@@ -27,13 +29,14 @@ SET time_zone = "+00:00";
 -- Estrutura para tabela `anuncios_carros`
 --
 
-CREATE TABLE `anuncios_carros` (
-  `id` int(12) NOT NULL,
+CREATE TABLE IF NOT EXISTS `anuncios_carros` (
+  `id` int(12) NOT NULL AUTO_INCREMENT,
   `estado_local` char(2) DEFAULT NULL,
+  `cidade` varchar(255) DEFAULT NULL,
   `marca` varchar(25) DEFAULT NULL,
   `modelo` varchar(75) DEFAULT NULL,
   `versao` varchar(40) DEFAULT NULL,
-  `carroceria` varchar(15) DEFAULT NULL,
+  `carroceria` int(2) DEFAULT NULL,
   `preco` decimal(11,2) DEFAULT NULL,
   `quilometragem` int(10) DEFAULT NULL,
   `ano_fabricacao` int(4) DEFAULT NULL,
@@ -48,7 +51,7 @@ CREATE TABLE `anuncios_carros` (
   `acentos_qtd` smallint(1) DEFAULT 5,
   `placa` char(7) DEFAULT NULL,
   `data_criacao` datetime NOT NULL DEFAULT current_timestamp(),
-  `cor` varchar(10) NOT NULL,
+  `cor` int(2) NOT NULL,
   `quant_proprietario` char(1) NOT NULL,
   `revisao` char(1) NOT NULL,
   `vistoria` char(1) NOT NULL,
@@ -59,8 +62,156 @@ CREATE TABLE `anuncios_carros` (
   `uso_anterior` char(1) NOT NULL,
   `aceita_troca` char(1) NOT NULL,
   `email` varchar(256) NOT NULL,
-  `telefone` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `telefone` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `placa` (`placa`),
+  KEY `carro_link_vendedor_id` (`id_vendedor`),
+  KEY `cor_fk` (`cor`),
+  KEY `carroceria_fk` (`carroceria`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `anuncios_carros`
+--
+
+INSERT INTO `anuncios_carros` (`id`, `estado_local`, `cidade`, `marca`, `modelo`, `versao`, `carroceria`, `preco`, `quilometragem`, `ano_fabricacao`, `ano_modelo`, `propulsao`, `combustivel`, `blindagem`, `id_vendedor`, `imagens`, `leilao`, `portas_qtd`, `acentos_qtd`, `placa`, `data_criacao`, `cor`, `quant_proprietario`, `revisao`, `vistoria`, `sinistro`, `ipva`, `licenciamento`, `estado_conservacao`, `uso_anterior`, `aceita_troca`, `email`, `telefone`) VALUES
+(13, NULL, NULL, 'ferrari', '488 spider', '3.9 V8 TURBO GASOLINA F1-DCT', 4, 3450000.00, 0, 2017, 2018, NULL, NULL, '0', 6, NULL, NULL, 4, 5, 'AAA1A11', '2025-09-30 18:50:26', 3, '2', '2', 'F', '0', 'D', 'D', '4', '', '0', 'kelwin@gmail.com', '(11) 11111-1111');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `carrocerias`
+--
+
+CREATE TABLE IF NOT EXISTS `carrocerias` (
+  `id` int(2) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `carrocerias`
+--
+
+INSERT INTO `carrocerias` (`id`, `nome`) VALUES
+(1, 'Hatchback'),
+(2, 'Sedan'),
+(3, 'SUV'),
+(4, 'Coupé'),
+(5, 'Perua'),
+(6, 'Van'),
+(7, 'Minivan'),
+(8, 'Picape'),
+(9, 'Conversível');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cores`
+--
+
+CREATE TABLE IF NOT EXISTS `cores` (
+  `id` int(2) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `cores`
+--
+
+INSERT INTO `cores` (`id`, `nome`) VALUES
+(1, 'Branco'),
+(2, 'Preto'),
+(3, 'Vermelho'),
+(4, 'Azul'),
+(5, 'Cinza'),
+(6, 'Prata'),
+(7, 'Vinho'),
+(8, 'Marrom'),
+(9, 'Laranja'),
+(10, 'Amarelo'),
+(11, 'Dourado'),
+(12, 'Verde'),
+(13, 'Bege');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `marcas`
+--
+
+CREATE TABLE IF NOT EXISTS `marcas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `value` varchar(50) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `value` (`value`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `marcas`
+--
+
+INSERT INTO `marcas` (`id`, `value`, `nome`) VALUES
+(1, 'abarth', 'Abarth'),
+(2, 'alfa', 'Alfa Romeo'),
+(3, 'aston', 'Aston Martin'),
+(4, 'audi', 'Audi'),
+(5, 'bentley', 'Bentley'),
+(6, 'bmw', 'BMW'),
+(7, 'bugatti', 'Bugatti'),
+(8, 'byd', 'BYD'),
+(9, 'cadillac', 'Cadillac'),
+(10, 'chevrolet', 'Chevrolet'),
+(11, 'chrysler', 'Chrysler'),
+(12, 'citroen', 'Citroën'),
+(13, 'corvette', 'Corvette'),
+(14, 'dacia', 'Dacia'),
+(15, 'dodge', 'Dodge'),
+(16, 'ferrari', 'Ferrari'),
+(17, 'fiat', 'Fiat'),
+(18, 'ford', 'Ford'),
+(19, 'genesis', 'Genesis'),
+(20, 'gmc', 'GMC'),
+(21, 'gwm', 'GWM'),
+(22, 'honda', 'Honda'),
+(23, 'hummer', 'Hummer'),
+(24, 'hyundai', 'Hyundai'),
+(25, 'infiniti', 'Infiniti'),
+(26, 'jaecoo', 'JAECOO'),
+(27, 'jaguar', 'Jaguar'),
+(28, 'jeep', 'Jeep'),
+(29, 'kia', 'Kia'),
+(30, 'koenigsegg', 'Koenigsegg'),
+(31, 'lamborghini', 'Lamborghini'),
+(32, 'lancia', 'Lancia'),
+(33, 'land', 'Land Rover'),
+(34, 'lexus', 'Lexus'),
+(35, 'lincoln', 'Lincoln'),
+(36, 'lotus', 'Lotus'),
+(37, 'maserati', 'Maserati'),
+(38, 'mazda', 'Mazda'),
+(39, 'mclaren', 'McLaren'),
+(40, 'mercedes', 'Mercedes-Benz'),
+(41, 'mini', 'MINI'),
+(42, 'mitsubishi', 'Mitsubishi'),
+(43, 'nissan', 'Nissan'),
+(44, 'omoda', 'Omoda'),
+(45, 'opel', 'Opel'),
+(46, 'peugeot', 'Peugeot'),
+(47, 'porsche', 'Porsche'),
+(48, 'ram', 'Ram'),
+(49, 'renault', 'Renault'),
+(50, 'rolls', 'Rolls-Royce'),
+(51, 'skoda', 'Skoda'),
+(52, 'smart', 'Smart'),
+(53, 'subaru', 'Subaru'),
+(54, 'suzuki', 'Suzuki'),
+(55, 'tesla', 'Tesla'),
+(56, 'toyota', 'Toyota'),
+(57, 'volkswagen', 'Volkswagen'),
+(58, 'volvo', 'Volvo');
 
 -- --------------------------------------------------------
 
@@ -68,8 +219,8 @@ CREATE TABLE `anuncios_carros` (
 -- Estrutura para tabela `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(6) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
   `nome` varchar(30) NOT NULL,
   `sobrenome` varchar(30) NOT NULL,
   `telefone` bigint(15) DEFAULT NULL,
@@ -77,8 +228,12 @@ CREATE TABLE `usuarios` (
   `email` varchar(120) NOT NULL,
   `senha` varchar(256) NOT NULL,
   `data_criacao_conta` datetime NOT NULL DEFAULT current_timestamp(),
-  `data_nascimento` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `data_nascimento` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `telefone` (`telefone`),
+  UNIQUE KEY `cpf` (`cpf`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
@@ -88,43 +243,6 @@ INSERT INTO `usuarios` (`id`, `nome`, `sobrenome`, `telefone`, `cpf`, `email`, `
 (6, 'Kelwin', 'Silva', NULL, NULL, 'kelwin@gmail.com', '1111AAAA', '2025-09-20 20:44:02', NULL);
 
 --
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `anuncios_carros`
---
-ALTER TABLE `anuncios_carros`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `placa` (`placa`),
-  ADD KEY `carro_link_vendedor_id` (`id_vendedor`);
-
---
--- Índices de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `telefone` (`telefone`),
-  ADD UNIQUE KEY `cpf` (`cpf`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `anuncios_carros`
---
-ALTER TABLE `anuncios_carros`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
 -- Restrições para tabelas despejadas
 --
 
@@ -132,7 +250,8 @@ ALTER TABLE `usuarios`
 -- Restrições para tabelas `anuncios_carros`
 --
 ALTER TABLE `anuncios_carros`
-  ADD CONSTRAINT `carro_link_vendedor_id` FOREIGN KEY (`id_vendedor`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `carroceria_fk` FOREIGN KEY (`carroceria`) REFERENCES `carrocerias` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cor_fk` FOREIGN KEY (`cor`) REFERENCES `cores` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
